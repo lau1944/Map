cee95c6eee4fdd0577d12763bce3d1106d6fb613
lau1944 <a954723219@gmail.com>
2019-01-26 14:26:06 +0800

    navigation button

---
+++ app/src/main/java/com/example/map/main_map.java
@@ -0,0 +1,41 @@
+package com.example.map;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class main_map extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_discover:
+                    mTextMessage.setText(R.string.title_discover);
+                    return true;
+                case R.id.navigation_fun:
+                    mTextMessage.setText(R.string.title_fun);
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText(R.string.title_setting);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+
+    }
+
+}
